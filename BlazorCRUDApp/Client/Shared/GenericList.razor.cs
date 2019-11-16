7f14ed30734790d418636908dc9308e67306fccb
Stephen J Naughton <steve@notaclue.net>
2019-11-16 21:47:00 +0000

    Added GenericList

---
+++ BlazorCRUDApp/Client/Shared/GenericList.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDApp.Client.Shared
+{
+    public partial class GenericList<TElement> : ComponentBase
+    {
+        [Parameter] 
+        public List<TElement> Elements { get; set; }
+
+        [Parameter] 
+        public RenderFragment NullTemplate { get; set; }
+
+        [Parameter] 
+        public RenderFragment EmptyTemplate { get; set; }
+
+        [Parameter] 
+        public RenderFragment WithElementsTemplate { get; set; }
+
+        [Parameter] 
+        public RenderFragment<TElement> WithIndividualElementTemplate { get; set; }
+    }
+}
